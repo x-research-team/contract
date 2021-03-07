9f60da0c3706d720c7fb102562a3b32ae359b8d0
Adel Urazov <Adel.Urazov@acronis.com>
2021-03-07 13:45:12 +0300

    Added Correlation ID fo Message

--- message.go
+++ message.go
@@ -1,7 +1,10 @@
 package contract
 
+import "github.com/google/uuid"
+
 // IMessage Сообщение сигнала ядра
 type IMessage interface {
+	ID() uuid.UUID
 	Route() string
 	Command() string
 	Data() string
